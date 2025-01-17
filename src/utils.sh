87fc0ba0d0d2767a4118a9ca12848f9a85650778
Mazhar Hussain <realmazharhussain@gmail.com>
2025-01-18 00:28:30 +0500

    Add attach_database.sh & improve connect_database.sh
    - Allow attaching multiple databases
    - Improve filename handling for connect_database.sh

---
+++ src/utils.sh
@@ -0,0 +1,23 @@
+
+# helpers/simple utilities
+
+real_path() { cd "$WORK_DIR" && realpath -Lm -- "$1" && cd - >/dev/null; }
+set_env() { echo "${ENV[$1]}"; }
+get_file() { echo "${FILE[$1]}"; }
+get_error_msg() { echo "${ERROR[$1]}"; }
+is_null() { [[ "$1" == "$NULL" ]] }         # is equal to defined null value
+is_true() { [[ "$1" != "0" ]] }              # NOT 0
+is() { [[ -n "$1" ]] }                      # non-empty
+not() { [[ -z "$1" ]] || [[ "$1" == '0' ]] || [[ "$1" == 'false' ]] } # returns positive if $1 is 0, 'false', or empty
+
+max_of() {
+  max=$1
+  shift
+
+  while [[ "$#" != 0 ]]; do
+    [[ $1 -gt $max ]] && max=$1
+    shift
+  done
+
+  echo "$max"
+}
